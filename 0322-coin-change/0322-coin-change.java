91b4e8831b3270f67781bd737edba4fabee5662b
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-08-03 19:08:43 +0530

    Time: 17 ms (48.58%), Space: 46.7 MB (40.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,34 +2,33 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int k = amount;
-
-        int[][] dp = new int[n][k+1];
+        int[] prev = new int[k+1];
+        int[] curr = new int[k+1];
 
         for(int t=0;t<=k;t++) {
             if(t%coins[0] == 0) {
-                dp[0][t] = t/coins[0];
+                prev[t] = t/coins[0];
             }
             else {
-                dp[0][t] = Integer.MAX_VALUE;
+                prev[t] = Integer.MAX_VALUE;
             }
         }
 
         for(int ind=1;ind<n;ind++) {
             for(int t=0;t<=k;t++) {
-                int notTake = dp[ind-1][t];
+                int notTake = prev[t];
 
                 int take = Integer.MAX_VALUE;
                 if(coins[ind] <= t) {
-                    int result = dp[ind][t-coins[ind]];
-
-                    if(result != Integer.MAX_VALUE) {
-                        take = 1 + result;
-                    }
+                    int result = curr[t-coins[ind]];
+                if(result != Integer.MAX_VALUE) {
+                    take = 1 + result;
                 }
-
-                dp[ind][t] = Math.min(take, notTake);
-            }     
+            }
+                curr[t] = Math.min(take, notTake);
+            }
+            prev = curr;
         }
-        return dp[n-1][k] == Integer.MAX_VALUE ? -1 : dp[n-1][k];
+        return prev[k] == Integer.MAX_VALUE ? -1 : prev[k];
     }
 }
