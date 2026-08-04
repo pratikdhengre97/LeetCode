b71317332797a7d854c55fab75e170ab69eed1a3
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-08-04 14:30:57 +0530

    Time: 4 ms (88.04%), Space: 43 MB (79.52%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -9,25 +9,19 @@ class Solution {
             totalSum += num;
         }
 
+
         if((totalSum-target) < 0 || (totalSum-target)%2 != 0) return 0;
 
         int subset = (totalSum-target)/2;
+        int[] dp = new int[subset+1];
 
-        int[][] dp = new int[n+1][subset+1];
+        dp[0] = 1;
 
-        for(int i=0;i<=n;i++) {
-            dp[i][0] = 1;
-        }
-
-        for(int ind=1;ind<=n;ind++) {
-            for(int t=0;t<=subset;t++) {
-                dp[ind][t] = dp[ind-1][t];
-
-                if(nums[ind-1] <= t) {
-                    dp[ind][t] += dp[ind-1][t-nums[ind-1]];
-                }
+        for(int num : nums) {
+            for(int j=subset;j>=num;j--) {
+                dp[j] += dp[j-num];
             }
         }
-        return dp[n][subset];
+        return dp[subset];
     }
 }
