67504a98d568a60465acf4499df7fc184232191c
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-08-04 12:22:50 +0530

    Time: 4 ms (88.04%), Space: 44.7 MB (53.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,45 @@
 class Solution {
-    public int f(int ind, int sum, int[] nums, int target) {
-        if(ind == nums.length) {
-            if(sum == target) {
-                return 1;
-            }
+    public int f(int ind,int[] nums, int target, int[][] dp) {
+
+        if(ind == 0) {
+            if(target == 0 && nums[0] == target) return 2;
+
+            if(target == 0 || nums[0] == target) return 1;
+
             return 0;
         }
 
-        int add = f(ind+1,sum+nums[ind], nums, target);
+        if(dp[ind][target] != -1) return dp[ind][target];
 
-        int subtract = f(ind+1, sum-nums[ind], nums, target);
 
-        return add + subtract;
+        int notPick = f(ind-1,nums, target, dp);
+
+        int pick = 0;
+
+        if(nums[ind] <= target) {
+            pick = f(ind-1,nums,target-nums[ind],dp);
+        }
+        return dp[ind][target] = pick + notPick;
     }
-
     public int findTargetSumWays(int[] nums, int target) {
-        return f(0, 0, nums, target);
+        int n = nums.length;
+        int t = target;
+
+        int totalSum = 0;
+        for(int num : nums) {
+            totalSum += num;
+        }
+
+        if((totalSum-target) < 0 || (totalSum-target)%2 != 0) return 0;
+
+        int subset = (totalSum-target)/2;
+
+        int[][] dp = new int[n][subset+1];
+
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return f(n-1,nums,subset,dp);
     }
 }
