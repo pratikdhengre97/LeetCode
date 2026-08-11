f0966b2601372269046197f5aea86464ffdb2361
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-08-11 10:40:54 +0530

    Time: 29 ms (21.84%), Space: 47.3 MB (16.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,34 +1,39 @@
 class Solution {
+    public int f(int ind, int[] coins, int target, int[][] dp) {
+
+        if(ind == 0) {
+            if(target%coins[ind] == 0) {
+                return target/coins[0];
+            }
+            return Integer.MAX_VALUE;
+        }
+
+        if(dp[ind][target] != -1) return dp[ind][target];
+
+        int notTake = f(ind-1,coins,target, dp);
+
+        int take = Integer.MAX_VALUE;
+
+        if(coins[ind] <= target) {
+            int result = f(ind, coins, target-coins[ind], dp);
+
+            if(result != Integer.MAX_VALUE) {
+                take = 1 + result;
+            }
+        }
+
+        return dp[ind][target] =  Math.min(take, notTake);
+
+    }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        int k = amount;
-        int[] prev = new int[k+1];
-        int[] curr = new int[k+1];
-
-        for(int t=0;t<=k;t++) {
-            if(t%coins[0] == 0) {
-                prev[t] = t/coins[0];
-            }
-            else {
-                prev[t] = Integer.MAX_VALUE;
-            }
+        int t = amount;
+        int[][] dp = new int[n][t+1];
+        for(int[] row : dp) {
+            Arrays.fill(row, -1);
         }
+        int ans = f(n-1,coins,amount, dp);
 
-        for(int ind=1;ind<n;ind++) {
-            for(int t=0;t<=k;t++) {
-                int notTake = prev[t];
-
-                int take = Integer.MAX_VALUE;
-                if(coins[ind] <= t) {
-                    int result = curr[t-coins[ind]];
-                if(result != Integer.MAX_VALUE) {
-                    take = 1 + result;
-                }
-            }
-                curr[t] = Math.min(take, notTake);
-            }
-            prev = curr;
-        }
-        return prev[k] == Integer.MAX_VALUE ? -1 : prev[k];
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
