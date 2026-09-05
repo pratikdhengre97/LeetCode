dbcb7d453bd103718c70e5ca07ee6783f6e0bc9d
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-09-05 23:53:45 +0530

    Time: 0 ms (100%), Space: 43 MB (19.17%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,19 @@
 class Solution {
-
     public int f(int ind, int[] nums, int[] dp) {
-        if(ind < 0 ) return 0;
+        if(ind < 0) return 0;
         if(ind == 0) return nums[0];
 
         if(dp[ind] != -1) return dp[ind];
 
         int pick = nums[ind] + f(ind-2,nums,dp);
-        int notPick = f(ind-1,nums,dp);
+        int notPick = f(ind-1, nums, dp);
 
         return dp[ind] = Math.max(pick, notPick);
     }
     public int rob(int[] nums) {
         int n = nums.length;
         int[] dp = new int[n];
-
-        
-            Arrays.fill(dp, -1);
-        
-        
+        Arrays.fill(dp, -1);
         return f(n-1, nums, dp);
     }
 }
