ef15a80cf3ccb05c8f9293a981b2db080a473862
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-09-12 12:43:45 +0530

    Time: 15 ms (59.97%), Space: 51.8 MB (6.51%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -3,15 +3,18 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
 
+
         int studentIndex = 0;
         int cookieIndex = 0;
+        int cnt = 0;
 
         while(studentIndex < g.length && cookieIndex < s.length) {
             if(s[cookieIndex] >= g[studentIndex]) {
+                cnt++;
                 studentIndex++;
             }
             cookieIndex++;
         }
-        return studentIndex;
+        return cnt;
     }
 }
