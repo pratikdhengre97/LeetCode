7ea137daffe70f96a9b6281fdb6ce2b854f9a3ed
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-09-16 21:47:13 +0530

    Time: 29 ms (22.42%), Space: 47 MB (30.72%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,38 +1,38 @@
 class Solution {
-    public int f(int ind, int[] coins, int target, int[][] dp) {
-
+    public int f(int ind, int[] coins, int t, int[][] dp) {
         if(ind == 0) {
-            if(target%coins[ind] == 0) {
-                return target/coins[0];
+            if(t%coins[ind] == 0) {
+                return t/coins[ind];
             }
             return Integer.MAX_VALUE;
         }
 
-        if(dp[ind][target] != -1) return dp[ind][target];
+        if(dp[ind][t] != -1) return dp[ind][t];
 
-        int notTake = f(ind-1,coins,target, dp);
-
+        int notTake = f(ind-1, coins, t, dp);
         int take = Integer.MAX_VALUE;
 
-        if(coins[ind] <= target) {
-            int result = f(ind, coins, target-coins[ind], dp);
+        if(coins[ind] <= t) {
+            int result = f(ind, coins, t-coins[ind], dp);
 
             if(result != Integer.MAX_VALUE) {
                 take = 1 + result;
             }
         }
-
-        return dp[ind][target] =  Math.min(take, notTake);
-
+        return dp[ind][t] = Math.min(take,notTake);
     }
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int t = amount;
+
         int[][] dp = new int[n][t+1];
+
         for(int[] row : dp) {
             Arrays.fill(row, -1);
         }
-        int ans = f(n-1,coins,amount, dp);
+
+        int ans = f(n-1, coins, t, dp);
+
 
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
