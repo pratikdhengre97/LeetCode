3151c2c93679f0d3bbc8b4a9b075ab877f4b92b8
pratikdhengre97 <126978979+pratikdhengre97@users.noreply.github.com>
2026-09-17 13:09:46 +0530

    Time: 1 ms (99.87%), Space: 43.2 MB (98.61%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.java
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.java
@@ -1,49 +1,48 @@
 class Solution {
-    public void backtrack(int ind, int parts, String s, StringBuilder curr, List<String> ans) {
+    public void backtrack(int index, int parts, String s, StringBuilder curr, List<String> ans) {
+
         if(parts == 4) {
-            if(ind == s.length()) {
+            if(index == s.length()) {
                 ans.add(curr.toString());
             }
             return;
         }
 
-        int remaining = s.length() - ind;
+        int remaining = s.length() - index;
 
-        if(remaining < (4-parts) || remaining > 3 * (4-parts)) {
+        if(remaining < (4-parts) || remaining > 3 *(4-parts)) {
             return;
         }
-
         int num = 0;
-        for(int i=ind;i<Math.min(ind+3, s.length());i++) {
+        for(int i=index;i<Math.min(index+3, s.length());i++) {
             num = num * 10 + s.charAt(i) - '0';
-            if(i > ind && s.charAt(ind) == '0') {
+
+            if(i > index && s.charAt(index) == '0') {
                 break;
             }
             if(num > 255) {
                 break;
             }
-
             int oldLength = curr.length();
 
             if(parts > 0) {
                 curr.append('.');
             }
-            curr.append(s,ind,i+1);
+            curr.append(s,index,i+1);
 
-            backtrack(i+1,parts+1,s,curr,ans);
-            
+            backtrack(i+1,parts+1,s, curr,ans);
+
             curr.setLength(oldLength);
         }
     }
     public List<String> restoreIpAddresses(String s) {
         List<String> ans = new ArrayList<>();
-
         if(s.length() < 4 || s.length() > 12) {
             return ans;
         }
 
         backtrack(0,0,s,new StringBuilder(), ans);
 
-        return ans;
+        return ans; 
     }
 }
